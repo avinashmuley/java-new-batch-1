f65b41151b4acc2d41b1e24da1b32a1065a35496
Avinash Muley <avimuley1317@gmail.com>
2023-05-11 18:45:38 +0530

    Initial commit

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,5 +2,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world from demo");
         System.out.println("welcome again");
+        System.out.println("welcome again");
     }
 }
